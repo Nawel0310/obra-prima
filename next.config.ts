38883eae8b99322f23df571d89050cf4364289b7
Nawel0310 <nbarbaro@frba.utn.edu.ar>
2026-05-12 00:09:48 -0300

    mejorado path de assets

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/obra-prima",
+  assetPrefix: "/obra-prima",
   images: {
     unoptimized: true,
   },
