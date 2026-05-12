76c80e256ff33a4264db1621ad789adcfb6c4aac
Nawel0310 <nbarbaro@frba.utn.edu.ar>
2026-05-12 00:22:34 -0300

    cambio final images

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+const basePath = "/obra-prima";
 
 const nextConfig: NextConfig = {
   output: "export",
